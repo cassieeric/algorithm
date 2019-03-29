a0ce996a752ec64fc97ab408a577531c8427f9db
Python_web crawler <31296722+cassieeric@users.noreply.github.com>
2019-03-29 23:27:28 +0800

    Create maopao_algorithm.cpp

---
+++ 冒泡排序算法（C程序）/maopao_algorithm.cpp
@@ -0,0 +1,36 @@
+# include <stdio.h>
+# include <stdlib.h>
+
+void sort(int * a, int len)
+{
+	int i, j, temp;
+	for(i=0; i<len-1; i++)
+	{
+		for(j=0; j<len-1-i; j++)
+		{
+			if(a[j] > a[j+1])  # 如果是>，则是升序排序；如果是<，则是降序排序
+			{
+				temp = a[j];
+				a[j] = a[j+1];
+				a[j+1] = temp;
+			}
+		}
+	}
+}
+
+int main(void)
+{
+	int a[6] = {10, 2, 8, -8, 11, 0};
+	int i = 0;
+
+	sort(a, 6);
+
+	for(i=0; i<6; i++)
+	{
+		printf("%d\n", a[i]);
+	}
+	printf("\n");
+
+	system("pause");
+	return 0;
+}
