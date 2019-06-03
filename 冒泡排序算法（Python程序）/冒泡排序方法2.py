b9d956a40ee76d1e90c05099c138bed89c827b1d
Python_web crawler <31296722+cassieeric@users.noreply.github.com>
2019-06-03 10:57:47 +0800

    Update 冒泡排序方法2.py

--- 冒泡排序算法（Python程序）/冒泡排序方法2.py
+++ 冒泡排序算法（Python程序）/冒泡排序方法2.py
@@ -1,8 +1,13 @@
 # -*- coding: utf-8 -*-
-a = [7, 11, 8, 12, 18, 2, 5, 9]
-for i in range(len(a)-1):
-    for j in range(len(a)-1-i):
-        if a[j] > a[j + 1]:
-            a[j], a[j + 1] = a[j + 1], a[j]
 
-print(a)
+def bubble_sort(list):
+    for i in range(len(list) - 1):
+        for j in range(len(list) - 1 - i):
+            if list[j] > list[j + 1]:
+                list[j], list[j + 1] = list[j + 1], list[j]
+
+if __name__ == '__main__':
+    list = [7, 11, 8, 12, 18, 2, 5, 9]
+    print(list)
+    bubble_sort(list)
+    print(list)
